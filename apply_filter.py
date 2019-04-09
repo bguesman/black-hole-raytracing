3f64b233136e907e040fd570bc49e953373ec533
Brad Guesman <bradguesman@brads-mbp.devices.brown.edu>
2019-04-09 16:04:33 -0400

    added in code to use numba, but currently it doens't work; some unsupported numpy feature

--- apply_filter.py
+++ apply_filter.py
@@ -3,10 +3,12 @@ import sys
 import cv2
 import numpy as np
 from scipy import interpolate
+from numba import jit
 
 NUM_ARGS = 4
 
 # Main entry point for script.
+# @jit
 def main():
     if len(sys.argv) < NUM_ARGS - 1:
       print("usage: python3 apply_filter.py <filter file> <image> <image to write to>")
@@ -18,25 +20,32 @@ def main():
         # Load the filter.
         filter = pickle.load(f)
         # Load the old image.
-        image_old = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+        image_old = cv2.imread(image_file)
+        # Split into color channels.
+        old_channels = [image_old[:,:,c] for c in range(3)]
         # Create the new image.
         image_new = np.ones(image_old.shape, dtype="uint8") * 255
 
-        # Create the interpolator. Here, the x coordinates
+        # Create the interpolators. Here, the x coordinates
         # are a range from 0-width, the y coordinates are from
         # 0-height, and the z values are specified by the old image.
+        # Why create a separate interpolator for each color?
+        # Because scipy's 2d interpolate function does not allow
+        # for interpolation of vector fields.
         width = image_old.shape[1]
         height = image_old.shape[0]
-        f = interpolate.RectBivariateSpline(np.arange(height),
-                                np.arange(width),
-                                image_old)
+        f = [interpolate.RectBivariateSpline(np.arange(height),
+                                np.arange(width), old_channels[c])
+                                for c in range(3)]
 
         # Now, we can use the interpolator together with the filter
         # lookup table to apply the filter and create the new image.
         for i in range(height):
             for j in range(width):
-                image_new[i, j] = f(filter[0, i * width + j],
-                                filter[1, i * width + j])
+                for c in range(3):
+                    image_new[i, j, c] = f[c](filter[0, i * width + j],
+                                    filter[1, i * width + j])
+
         cv2.imwrite(to_write_to, image_new)
 
 # Run main.
