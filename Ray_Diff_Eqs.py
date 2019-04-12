2aa8cc6736215461b6a6e7fe3bc231fcbecfc1f8
amlawson98 <32847387+amlawson98@users.noreply.github.com>
2019-04-12 14:36:34 -0400

    added diff_eqs

---
+++ Ray_Diff_Eqs.py
@@ -0,0 +1,48 @@
+#We have the main differential equations
+
+# t'' = -2GM/(r(r-2GM)) * r' *t'
+#r'' = -(GM/r^3)*(r-2GM)*(t')^2 + (GM/(r(r-@GM)))
+import numpy as np
+
+def sin(theta):
+	return np.sin(theta)
+def cos(theta):
+	return np.cos(theta)
+G=1
+M=1
+
+def sphere_to_cartesian(vec):
+	t, r, thet, phi = tuple(vec)
+	x= r*sin(thet)*cos(phi)
+	y= r*sin(thet)*sin(phi)
+	z= r*cos(thet)
+	return np.array([t, x, y, z])
+
+def cartesian_to_sphere(vec):
+	t, x, y, z = tuple(vec)
+	r= (x**2 + y**2 + z**2)**.5
+	thet= np.arccos(z/r)
+	phi= np.arctan2(y,x)
+	return np.array([t, r, thet, phi])
+
+class photon(object):
+	def __init__(pos, pos_):
+		self.pos0 = copy.copy(pos)
+		self.pos
+		
+
+def pos__(pos, pos_):
+	t, r, thet, phi = tuple(pos)
+	t_, r_, thet_, phi_ = tuple(pos_)
+	t__ = ((-2*G*M)/(r*(r-2*G*M)))*r_*t_
+	
+	r__ = -(G*M/(r**3))*(r-2*G*M)*(t_**2) + (G*M/(r*(r-2*G*M)))*(r_**2) + (r-2*G*M)*((thet_**2) + (sin(thet)*phi_)**2)
+
+	thet__ = (-2/r)*thet_*r_ + sin(thet)*cos(thet)*(phi_**2)
+
+	phi__ = (-2/r)*phi_*r_ + 2*(cos(thet)/sin(thet))*thet_*phi_
+
+	return np.array([t__, r__, thet__, phi__])
+
+print(cartesian_to_sphere(sphere_to_cartesian(np.array([1,2,3,4]))))
+print(sphere_to_cartesian(cartesian_to_sphere(np.array([1,2,3,4]))))
