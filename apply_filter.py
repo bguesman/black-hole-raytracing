951436bc13f8f05a9fbbc624b2a9f34d0374d091
Brad Guesman <bradguesman@brads-mbp.devices.brown.edu>
2019-04-16 18:10:43 -0400

    black hole should be black now

--- apply_filter.py
+++ apply_filter.py
@@ -62,7 +62,10 @@ def main():
             for j in range(width):
                 for c in range(3):
                     print(filter[1, i * width + j])
-                    image_new[i, j, c] = f[c](filter[0, i * width + j],
+                    if (filter[1, i * width + j] < 0):
+                        image_new[i, j, c] = 0
+                    else:
+                        image_new[i, j, c] = f[c](filter[0, i * width + j],
                                     filter[1, i * width + j])
         cv2.imwrite(to_write_to, image_new)
 
