d0940aa9b71b3a2c5f1975fa570b0846b2803afa
Brad Guesman <bradguesman@brads-mbp.devices.brown.edu>
2019-04-16 21:50:48 -0400

    got some better interpolation code from the internet and used that

--- apply_filter.py
+++ apply_filter.py
@@ -9,6 +9,37 @@ from numba import jit
 
 NUM_ARGS = 4
 
+# Pulled from the internet because scipy sucks.
+def bilinear_interpolation(x, y, points):
+    '''Interpolate (x,y) from values associated with four points.
+
+    The four points are a list of four triplets:  (x, y, value).
+    The four points can be in any order.  They should form a rectangle.
+
+        >>> bilinear_interpolation(12, 5.5,
+        ...                        [(10, 4, 100),
+        ...                         (20, 4, 200),
+        ...                         (10, 6, 150),
+        ...                         (20, 6, 300)])
+        165.0
+
+    '''
+    # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
+
+    points = sorted(points)               # order points by x, then by y
+    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
+
+    if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+        raise ValueError('points do not form a rectangle')
+    if not x1 <= x <= x2 or not y1 <= y <= y2:
+        raise ValueError('(x, y) not within the rectangle')
+
+    return (q11 * (x2 - x) * (y2 - y) +
+            q21 * (x - x1) * (y2 - y) +
+            q12 * (x2 - x) * (y - y1) +
+            q22 * (x - x1) * (y - y1)
+           ) / ((x2 - x1) * (y2 - y1) + 0.0)
+
 # Main entry point for script.
 # @jit
 # python apply_filter.py filters/width125_height125.pkl test-images/avolovich_photo_.jpg test-images/avolovich.jpg
@@ -38,9 +69,9 @@ def main():
         # for interpolation of vector fields.
         width = image_old.shape[1]
         height = image_old.shape[0]
-        f = [interpolate.RectBivariateSpline(np.arange(height),
-                                np.arange(width), old_channels[c])
-                                for c in range(3)]
+        # f = [interpolate.RectBivariateSpline(np.arange(height),
+        #                         np.arange(width), old_channels[c])
+        #                         for c in range(3)]
 
         # row_mean = np.mean((filter[0])[filter[0] != np.amax(filter[0])])
         # col_mean = np.mean((filter[1])[filter[1] != np.amin(filter[1])])
@@ -79,11 +110,19 @@ def main():
         for i in tqdm(range(height)):
             for j in range(width):
                 for c in range(3):
-                    if (filter[1, i * width + j] < 0):
+                    if (filter[1, i * width + j] < 0 or filter[1, i * width + j] >= width
+                        or filter[0, i * width + j] < 0 or filter[0, i * width + j] >= height):
                         image_new[i, j, c] = 0
                     else:
-                        image_new[i, j, c] = f[c]((filter[0, i * width + j]),
-                                    (filter[1, i * width + j]))
+                        xl = int((filter[1, i * width + j]))
+                        xh = xl + 1
+                        yl = int((filter[0, i * width + j]))
+                        yh = yl + 1
+                        chan = old_channels[c]
+                        points = [(xl, yl, chan[yl, xl]), (xl, yh, chan[yh % height, xl]),
+                                    (xh, yl, chan[yl, xh % width]), (xh, yh, chan[yh % height, xh % width])]
+                        image_new[i, j, c] = bilinear_interpolation(filter[1, i * width + j],
+                            filter[0, i * width + j], points)
         cv2.imwrite(to_write_to, image_new)
 
 # Run main.
